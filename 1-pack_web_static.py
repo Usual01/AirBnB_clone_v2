e94c7e4e8d18a84d5cb837318683a47f1f156baf
michael <mikedami001@gmail.com>
2023-09-06 11:46:31 -0700

    l[

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,10 @@
-#!/usr/bin/python3
-"""Generates a .tgz archive from the contents of the web_static folder."""
-from fabric.api import local
-import time
-
-
-def do_pack():
-    """Generate an tgz archive from web_static folder"""
-    try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(time.strftime("%Y%m%d%H%M%S")))
-        return ("versions/web_static_{}.tgz".format(time.
-                                                    strftime("%Y%m%d%H%M%S")))
-    except:
-        return None
+#!/usr/bin/env bash
+# sets up web servers for the deployment of web_static
+apt-get update -y > /dev/null 2>&1
+apt-get install nginx -y > /dev/null 2>&1
+mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+echo "Hello from web_static" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '/^\tserver_name.*/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart > /dev/null 2>&1
