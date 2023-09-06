805cfc5f9f4cde03452b58071c71110fa63b2c37
michael <mikedami001@gmail.com>
2023-09-06 11:50:05 -0700

    af

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env bash
-# Setup a web servers for the deployment of web_static.
-apt update -y
-apt install -y nginx
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "<!DOCTYPE html>
-<html>
-  <head>
-  </head>
-  <body>
-    <p>Nginx server test</p>
-  </body>
-</html>" | tee /data/web_static/releases/test/index.html
+# sets up web servers for the deployment of web_static
+apt-get update -y > /dev/null 2>&1
+apt-get install nginx -y > /dev/null 2>&1
+mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+echo "Hello from web_static" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
-sudo service nginx restart
+chown -R ubuntu:ubuntu /data/
+sed -i '/^\tserver_name.*/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart > /dev/null 2>&1
